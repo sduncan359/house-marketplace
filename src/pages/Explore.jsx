54f53c6932163fdc5deb26ca036078d4d4a6b0fa
Steven Duncan <Steven.Duncan@paccar.com>
2023-02-06 14:13:54 -0800

    start work on profile page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,7 +1,39 @@
+/* prisma, ES7 React/Redux..., ESLint extensions needed */
+
+import { Link } from "react-router-dom";
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
+
 function Explore() {
   return (
-    <div>Explore</div>
-  )
+    <div className="explore">
+      <header className="pageHeader">Explore</header>
+
+      <main>
+        {/* Slider */}
+
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              src={rentCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for rent</p>
+          </Link>
+          <Link to="/category/sale">
+            <img
+              src={sellCategoryImage}
+              alt="sell"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for sale</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default Explore
+export default Explore;
