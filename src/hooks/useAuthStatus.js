f91286bbbc65ba996e4e20b57570b9e60a2586d8
Steve Duncan <67704665+sduncan-propack@users.noreply.github.com>
2023-02-06 18:02:33 -0800

    OAuth

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,19 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const useAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [checkingStatus, setCheckingStatus] = useState(true);
+  const isMounted = useRef(true);
 
   useEffect(() => {
-    const auth = getAuth()
-    onAuthStateChanged(auth, (user) => {
+    if (isMounted) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
         if (user) {
-            setLoggedIn(true)            
+          setLoggedIn(true);
         }
-        setCheckingStatus(false)
-    });
-  });
+        setCheckingStatus(false);
+      });
+    }
+
+    return () => {
+      isMounted.current = false
+    }
+  }, [isMounted]);
 
   return { loggedIn, checkingStatus };
 };
