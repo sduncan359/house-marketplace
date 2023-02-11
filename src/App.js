9bd5b8d93de57f5a26c22cf603d7a4b26a61b3c5
Steve Duncan <67704665+sduncan-propack@users.noreply.github.com>
2023-02-10 18:23:00 -0800

    start listing page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
-import Explore from './pages/Explore'
-import ForgotPassword from './pages/ForgotPassword'
-import Offers from './pages/Offers'
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 import Category from "./pages/Category";
-import Profile from './pages/Profile'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
 
-function App() {   
+function App() {
   return (
     <>
       <Router>
@@ -27,6 +28,10 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
         </Routes>
         <Navbar />
       </Router>
--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -3,7 +3,7 @@ import { ReactComponent as DeleteIcon } from "../assets/svg/deleteIcon.svg";
 import bedIcon from "../assets/svg/bedIcon.svg";
 import bathtubIcon from "../assets/svg/bathtubIcon.svg";
 
-function ListingItem({ listing, id, onDelete }) {
+function ListingItem({ listing, id, onDelete }) {  
   return (
     <li className="categoryListing">
       <Link
@@ -11,7 +11,7 @@ function ListingItem({ listing, id, onDelete }) {
         className="categoryListingLink"
       >
         <img
-          src={listing.imageUrls[0]}
+          src={listing.imgUrls[0]}
           alt={listing.name}
           className="categoryListingImg"
         />
