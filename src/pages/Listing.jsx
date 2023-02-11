4ba06007238779acc350d5fd3e1c576efe908c86
Steve Duncan <67704665+sduncan-propack@users.noreply.github.com>
2023-02-11 10:41:08 -0800

    add listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -4,33 +4,104 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { db } from "../firebase.config";
-import shareIcon from '../assets/svg/shareIcon.svg'
+import shareIcon from "../assets/svg/shareIcon.svg";
 
 function Listing() {
-    const [listing, setListing] = useState(null)
-    const [loading, setLoading] = useState(true);
-    const [shareLinkCopied, setShareLinkCopied] = useState(false);
-    
-    const navigate = useNavigate()
-    const params = useParams();
-    const auth = getAuth()
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
-    useEffect(() => {
-        const fetchListing = async () => {
-            const docRef = doc(db, 'listings', params.listingId)
-            const docSnap = await getDoc(docRef)
+  const navigate = useNavigate();
+  const params = useParams();
+  const auth = getAuth();
 
-            if (docSnap.exists) {
-                console.log(docSnap.data())
-                setListing(docSnap.data())
-                setLoading(false)
-            }
-        }
+  useEffect(() => {
+    const fetchListing = async () => {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
 
-        fetchListing()
-    },[navigate, params.listingId])
+      if (docSnap.exists) {
+        console.log(docSnap.data());
+        setListing(docSnap.data());
+        setLoading(false);
+      }
+    };
 
-    return <div>LISTING</div>;
+    fetchListing();
+  }, [navigate, params.listingId]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <main>
+      {/* SLIDER */}
+      <div
+        className="shareIconDiv"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <img src={shareIcon} alt="" />
+      </div>
+
+      {shareLinkCopied && <p className="linkCopied">Link Copied!</p>}
+
+      <div className="listingDetails">
+        <p className="listingName">
+          {listing.name} - $
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </p>
+        <p className="listingLocation">{listing.location}</p>
+        <p className="listingType">
+          For {listing.type === "rent" ? "Rent" : "Sale"}
+        </p>
+        {listing.offer && (
+          <p className="discountedPrice">
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+
+        <ul className="listingDetailsList">
+          <li>
+            {listing.bedrooms > 1
+              ? `${listing.bedrooms} Bedrooms`
+              : "1 Bedroom"}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} Bathrooms`
+              : "1 Bathroom"}
+          </li>
+          <li>{listing.parking && "Parking Spot"}</li>
+          <li>{listing.furnished && "Furnished"}</li>
+        </ul>
+
+        <p className="listingLocationTitle">Location</p>
+        {/* MAP */}
+
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+            className="PrimaryButton"
+          >
+            Contact Landlord
+          </Link>
+        )}
+      </div>
+    </main>
+  );
 }
 
 export default Listing;
